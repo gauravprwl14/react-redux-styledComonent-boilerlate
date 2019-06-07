1031af4f4d91ea6eea2026cb1a4bd4d3cd8723b6
gaurav porwal <gauravprwl14@gmail.com>
2019-06-08 03:02:48 +0530

    setup the eslint

--- src/App.js
+++ src/App.js
@@ -4,22 +4,19 @@ import "./App.css";
 
 function App() {
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+                    Edit
+            <code>src/App.js</code>
+                    and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org">
                     Learn React
-                </a>
-            </header>
-        </div>
+          </a>
+        </header>
+      </div>
     );
 }
 
